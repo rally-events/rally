b80855e6e08efe8f44ec4eecdd59dfd5f92cb9e4
DominicClerici <dclerici77@gmail.com>
2025-07-02 06:01:56 -0400

    update migrations location

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config()
 
 export default defineConfig({
   schema: "./src/schema/*.ts",
-  out: "./drizzle",
+  out: "../../supabase/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
